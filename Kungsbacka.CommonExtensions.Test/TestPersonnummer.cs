e1642cc08617517578e98a86b46adb507ebe66ff
Jonas Sjömark <jonas.sjomark@kungsbacka.se>
2026-03-24 13:51:36 +0100

    Use Therory/InlineData in tests

--- Kungsbacka.CommonExtensions.Test/TestPersonnummer.cs
+++ Kungsbacka.CommonExtensions.Test/TestPersonnummer.cs
@@ -1,62 +1,28 @@
 using Xunit;
 using System;
-using System.Collections.Generic;
 
 namespace Kungsbacka.CommonExtensions.Tests
 {
-
-#pragma warning disable CS0659 // Type overrides Object.Equals(object o) but does not override Object.GetHashCode()
-    internal class TestResult
-#pragma warning restore CS0659 // Type overrides Object.Equals(object o) but does not override Object.GetHashCode()
-    {
-        public string LongForm { get; set; }
-        public string ShortForm { get; set; }
-        public string Original { get; set; }
-        public bool IsTemporary { get; set; }
-        public DateTime BirthDate { get; set; }
-
-        public override bool Equals(object obj)
-        {
-            return obj is TestResult result &&
-                   LongForm == result.LongForm &&
-                   ShortForm == result.ShortForm &&
-                   Original == result.Original &&
-                   IsTemporary == result.IsTemporary &&
-                   BirthDate == result.BirthDate;
-        }
-    }
-
     public class TestPersonnummer
     {
-        readonly IReadOnlyDictionary<string, TestResult> ValidTestCases = new Dictionary<string, TestResult>()
+        [Theory]
+        [InlineData("194912232594",  "194912232594", "491223-2594", false, "19491223")]
+        [InlineData("19491223TF94",  "19491223TF94", "491223-TF94", true,  "19491223")]
+        [InlineData("19491223-2594", "194912232594", "491223-2594", false, "19491223")]
+        [InlineData("19491223-TF94", "19491223TF94", "491223-TF94", true,  "19491223")]
+        [InlineData("4912232594",    "194912232594", "491223-2594", false, "19491223")]
+        [InlineData("491223TF94",    "19491223TF94", "491223-TF94", true,  "19491223")]
+        [InlineData("491223-2594",   "194912232594", "491223-2594", false, "19491223")]
+        [InlineData("491223-TF94",   "19491223TF94", "491223-TF94", true,  "19491223")]
+        [InlineData("194912832591",  "194912832591", "491283-2591", false, "19491223")]
+        public void TestValid(string input, string longForm, string shortForm, bool isTemporary, string birthDate)
         {
-            { "194912232594",   new TestResult() {Original = "194912232594" , LongForm = "194912232594", ShortForm = "491223-2594", IsTemporary = false, BirthDate = DateTime.ParseExact("19491223", "yyyyMMdd", null) } },
-            { "19491223TF94",   new TestResult() {Original = "19491223TF94" , LongForm = "19491223TF94", ShortForm = "491223-TF94", IsTemporary = true , BirthDate = DateTime.ParseExact("19491223", "yyyyMMdd", null) } },
-            { "19491223-2594",  new TestResult() {Original = "19491223-2594", LongForm = "194912232594", ShortForm = "491223-2594", IsTemporary = false, BirthDate = DateTime.ParseExact("19491223", "yyyyMMdd", null) } },
-            { "19491223-TF94",  new TestResult() {Original = "19491223-TF94", LongForm = "19491223TF94", ShortForm = "491223-TF94", IsTemporary = true , BirthDate = DateTime.ParseExact("19491223", "yyyyMMdd", null) } },
-            { "4912232594",     new TestResult() {Original = "4912232594"   , LongForm = "194912232594", ShortForm = "491223-2594", IsTemporary = false, BirthDate = DateTime.ParseExact("19491223", "yyyyMMdd", null) } },
-            { "491223TF94",     new TestResult() {Original = "491223TF94"   , LongForm = "19491223TF94", ShortForm = "491223-TF94", IsTemporary = true , BirthDate = DateTime.ParseExact("19491223", "yyyyMMdd", null) } },
-            { "491223-2594",    new TestResult() {Original = "491223-2594"  , LongForm = "194912232594", ShortForm = "491223-2594", IsTemporary = false, BirthDate = DateTime.ParseExact("19491223", "yyyyMMdd", null) } },
-            { "491223-TF94",    new TestResult() {Original = "491223-TF94"  , LongForm = "19491223TF94", ShortForm = "491223-TF94", IsTemporary = true , BirthDate = DateTime.ParseExact("19491223", "yyyyMMdd", null) } },
-            { "194912832591",   new TestResult() {Original = "194912832591" , LongForm = "194912832591", ShortForm = "491283-2591", IsTemporary = false, BirthDate = DateTime.ParseExact("19491223", "yyyyMMdd", null) } },
-        };
-
-        [Fact]
-        public void TestValid()
-        {
-            foreach (string key in ValidTestCases.Keys)
-            {
-                Personnummer personnummer = new Personnummer(key);
-                TestResult actual = new TestResult()
-                {
-                    Original = personnummer.Original,
-                    LongForm = personnummer.LongForm,
-                    ShortForm = personnummer.ShortForm,
-                    IsTemporary = personnummer.IsTemporary,
-                    BirthDate = personnummer.BirthDate
-                };
-                Assert.True(ValidTestCases[key].Equals(actual), key);
-            }
+            Personnummer personnummer = new(input);
+            Assert.Equal(input,      personnummer.Original);
+            Assert.Equal(longForm,   personnummer.LongForm);
+            Assert.Equal(shortForm,  personnummer.ShortForm);
+            Assert.Equal(isTemporary, personnummer.IsTemporary);
+            Assert.Equal(DateTime.ParseExact(birthDate, "yyyyMMdd", null), personnummer.BirthDate);
         }
 
         [Fact]
@@ -65,52 +31,23 @@ namespace Kungsbacka.CommonExtensions.Tests
             Assert.Throws<ArgumentNullException>(() => new Personnummer(null));
         }
 
-        [Fact]
-        public void TestEmpty()
+        [Theory]
+        [InlineData("          ")]
+        [InlineData("123456789")]
+        [InlineData("12345-6789")]
+        [InlineData("abcdefghijkl")]
+        [InlineData("491223tf94")]
+        [InlineData("194912232584")]
+        [InlineData("8013015212")]
+        public void TestInvalidFormat(string input)
         {
-            Assert.Throws<FormatException>(() => new Personnummer("          "));
-        }
-
-        [Fact]
-        public void TestBad1()
-        {
-            Assert.Throws<FormatException>(() => new Personnummer("123456789"));
-        }
-
-        [Fact]
-        public void TestBad2()
-        {
-            Assert.Throws<FormatException>(() => new Personnummer("12345-6789"));
-        }
-
-        [Fact]
-        public void TestBad3()
-        {
-            Assert.Throws<FormatException>(() => new Personnummer("abcdefghijkl"));
-        }
-
-        [Fact]
-        public void TestBad4()
-        {
-            Assert.Throws<FormatException>(() => new Personnummer("491223tf94"));
-        }
-
-        [Fact]
-        public void TestBadCheckDigit()
-        {
-            Assert.Throws<FormatException>(() => new Personnummer("194912232584"));
-        }
-
-        [Fact]
-        public void TestBadBirthDate()
-        {
-            Assert.Throws<FormatException>(() => new Personnummer("8013015212"));
+            Assert.Throws<FormatException>(() => new Personnummer(input));
         }
 
         [Fact]
         public void TestFutureBirthDate()
         {
-            Personnummer personnummer = new Personnummer("801301015218");
+            Personnummer personnummer = new("801301015218");
             Assert.Equal(DateTime.Parse("8013-01-1"), personnummer.BirthDate);
         }
     }
--- Kungsbacka.CommonExtensions.Test/TestStringExtensions.cs
+++ Kungsbacka.CommonExtensions.Test/TestStringExtensions.cs
@@ -4,160 +4,150 @@ namespace Kungsbacka.CommonExtensions.Tests
 {
     public class TestStringExtensions
     {
-        [Fact]
-        public void TestIndexOfUnescaped()
+        [Theory]
+        [InlineData(null,        -1)]
+        [InlineData("",          -1)]
+        [InlineData(".",          0)]
+        [InlineData("A.",         1)]
+        [InlineData(@"\.",       -1)]
+        [InlineData(@"\\.",       2)]
+        [InlineData(@"A\.",      -1)]
+        [InlineData(@"A\\.",      3)]
+        [InlineData(@"\\\\.",     4)]
+        [InlineData(@"A\\\\.",    5)]
+        [InlineData(@"\\\.",     -1)]
+        [InlineData(@"A\\\.",    -1)]
+        [InlineData(@"AA\.",     -1)]
+        [InlineData(@"\",        -1)]
+        [InlineData(@"A\.A\\.",   6)]
+        public void TestIndexOfUnescaped(string input, int expected)
         {
-            Assert.Equal(-1, ((string)null).IndexOfUnescaped('.'));  // #0
-            Assert.Equal(-1, @""           .IndexOfUnescaped('.'));  // #1
-            Assert.Equal(-1, @""           .IndexOfUnescaped('.'));  // #1
-            Assert.Equal( 0, @"."          .IndexOfUnescaped('.'));  // #2
-            Assert.Equal( 1, @"A."         .IndexOfUnescaped('.'));  // #3
-            Assert.Equal(-1, @"\."         .IndexOfUnescaped('.'));  // #4
-            Assert.Equal( 2, @"\\."        .IndexOfUnescaped('.'));  // #5
-            Assert.Equal(-1, @"A\."        .IndexOfUnescaped('.'));  // #6
-            Assert.Equal( 3, @"A\\."       .IndexOfUnescaped('.'));  // #7
-            Assert.Equal( 4, @"\\\\."      .IndexOfUnescaped('.'));  // #8
-            Assert.Equal( 5, @"A\\\\."     .IndexOfUnescaped('.'));  // #9
-            Assert.Equal(-1, @"\\\."       .IndexOfUnescaped('.'));  // #10
-            Assert.Equal(-1, @"A\\\."      .IndexOfUnescaped('.'));  // #11
-            Assert.Equal(-1, @"AA\."       .IndexOfUnescaped('.'));  // #12
-            Assert.Equal(-1, @"\"          .IndexOfUnescaped('.'));  // #13
-            Assert.Equal( 6, @"A\.A\\."    .IndexOfUnescaped('.'));  // #14
+            Assert.Equal(expected, input.IndexOfUnescaped('.'));
         }
 
-        [Fact]
-        public void TestLastIndexOfUnescaped()
+        [Theory]
+        [InlineData(null,          -1)]
+        [InlineData(@"ABC.123",     3)]
+        [InlineData(@"ABC\.123",   -1)]
+        [InlineData(@"ABC.123.",    7)]
+        [InlineData(@"ABC\.123.",   8)]
+        [InlineData(@"\.",         -1)]
+        [InlineData(@".\.\\\.",     0)]
+        [InlineData("",            -1)]
+        [InlineData(".",            0)]
+        [InlineData(@".\.",         0)]
+        [InlineData(@"\",          -1)]
+        [InlineData(@".\",          0)]
+        [InlineData(@"\\..",        3)]
+        [InlineData(@"\\...",       4)]
+        public void TestLastIndexOfUnescaped(string input, int expected)
         {
-            Assert.Equal(-1, ((string)null).LastIndexOfUnescaped('.'));  // #0
-            Assert.Equal( 3, @"ABC.123"    .LastIndexOfUnescaped('.'));  // #1
-            Assert.Equal(-1, @"ABC\.123"   .LastIndexOfUnescaped('.'));  // #2
-            Assert.Equal( 7, @"ABC.123."   .LastIndexOfUnescaped('.'));  // #3
-            Assert.Equal( 8, @"ABC\.123."  .LastIndexOfUnescaped('.'));  // #4
-            Assert.Equal(-1, @"\."         .LastIndexOfUnescaped('.'));  // #5
-            Assert.Equal( 0, @".\.\\\."    .LastIndexOfUnescaped('.'));  // #6
-            Assert.Equal(-1, @""           .LastIndexOfUnescaped('.'));  // #7
-            Assert.Equal( 0, @"."          .LastIndexOfUnescaped('.'));  // #8
-            Assert.Equal( 0, @".\."        .LastIndexOfUnescaped('.'));  // #9
-            Assert.Equal(-1, @"\"          .LastIndexOfUnescaped('.'));  // #10
-            Assert.Equal( 0, @".\"         .LastIndexOfUnescaped('.'));  // #11
-            Assert.Equal( 3, @"\\.."       .LastIndexOfUnescaped('.'));  // #12
-            Assert.Equal( 4, @"\\..."      .LastIndexOfUnescaped('.'));  // #13
+            Assert.Equal(expected, input.LastIndexOfUnescaped('.'));
         }
 
-        [Fact]
-        public void TestRemoveDiacritics()
+        [Theory]
+        [InlineData(null,    "")]
+        [InlineData("",      "")]
+        [InlineData("åäöÅÄÖ", "aaoAAO")]
+        [InlineData("ñéí´",  "nei´")]
+        [InlineData("ßÆØ",   "ßÆØ")]
+        public void TestRemoveDiacritics(string input, string expected)
         {
-            Assert.Equal(""      , ((string)null).RemoveDiacritic());  // #0
-            Assert.Equal(""      , ""            .RemoveDiacritic());  // #1
-            Assert.Equal("aaoAAO", "åäöÅÄÖ"      .RemoveDiacritic());  // #2
-            Assert.Equal("nei´"  , "ñéí´"        .RemoveDiacritic());  // #3
-            Assert.Equal("ßÆØ"   , "ßÆØ"         .RemoveDiacritic());  // #4
+            Assert.Equal(expected, input.RemoveDiacritic());
         }
 
-        [Fact]
-        public void TestRemoveRepeating()
+        [Theory]
+        [InlineData(null,         "",    "")]
+        [InlineData("aabbcc",     "ac",  "abbc")]
+        [InlineData("--.-.--..-", ".-",  "-.-.-.-")]
+        [InlineData("   ",        " ",   " ")]
+        public void TestRemoveRepeating(string input, string chars, string expected)
         {
-            Assert.Equal("", ((string)null).RemoveRepeating(new char[] { }));
-            Assert.Equal("abbc", "aabbcc".RemoveRepeating(new char[] { 'a', 'c' }));
-            Assert.Equal("-.-.-.-", "--.-.--..-".RemoveRepeating(new char[] { '.', '-' }));
-            Assert.Equal(" ", "   ".RemoveRepeating(new char[] { ' ' }));
+            Assert.Equal(expected, input.RemoveRepeating(chars.ToCharArray()));
         }
 
-        [Fact]
-        public void TestGetNumberSuffix()
+        [Theory]
+        [InlineData(null,            -1)]
+        [InlineData("somestring123", 123)]
+        [InlineData("",              -1)]
+        [InlineData("123somestring", -1)]
+        public void TestGetNumberSuffix(string input, int expected)
         {
-            Assert.Equal(-1, ((string)null).GetNumberSuffix());
-            Assert.Equal(123, "somestring123".GetNumberSuffix());
-            Assert.Equal(-1, "".GetNumberSuffix());
-            Assert.Equal(-1, "123somestring".GetNumberSuffix());
+            Assert.Equal(expected, input.GetNumberSuffix());
         }
 
-        [Fact]
-        public void TestGetProperFirstName()
+        [Theory]
+        [InlineData(null,                    "00", "")]
+        [InlineData("Givenname",             "10", "Givenname")]
+        [InlineData("Name1 Name2 Name3",     "10", "Name1")]
+        [InlineData("Name1 Name2 Name3",     "20", "Name2")]
+        [InlineData("Name1 Name2 Name3",     "12", "Name1 Name2")]
+        [InlineData("Name1 Name2-Name3 Name4", "12", "Name1 Name2")]
+        [InlineData("Name1 Name2-Name3 Name4", "14", "Name1 Name4")]
+        [InlineData("Name1 Name2-Name3 Name4", "23", "Name2-Name3")]
+        [InlineData("Name1 Name2-Name3 Name4", "78", "")]
+        public void TestGetProperFirstName(string input, string code, string expected)
         {
-            string properName;
-
-            properName = ((string)null).GetProperFirstName("00");
-            Assert.Equal("", properName);  // #0
-
-            properName = "Givenname".GetProperFirstName("10");
-            Assert.Equal("Givenname", properName);  // #1
-
-            properName = "Name1 Name2 Name3".GetProperFirstName("10");
-            Assert.Equal("Name1", properName);  // #2
-
-            properName = "Name1 Name2 Name3".GetProperFirstName("20");
-            Assert.Equal("Name2", properName);  // #3
-
-            properName = "Name1 Name2 Name3".GetProperFirstName("12");
-            Assert.Equal("Name1 Name2", properName);  // #4
-
-            properName = "Name1 Name2-Name3 Name4".GetProperFirstName("12");
-            Assert.Equal("Name1 Name2", properName);  // #5
-
-            properName = "Name1 Name2-Name3 Name4".GetProperFirstName("14");
-            Assert.Equal("Name1 Name4", properName);  // #6
-
-            properName = "Name1 Name2-Name3 Name4".GetProperFirstName("23");
-            Assert.Equal("Name2-Name3", properName);  // #7
-
-            properName = "Name1 Name2-Name3 Name4".GetProperFirstName("78");
-            Assert.Equal("", properName);  // #8
+            Assert.Equal(expected, input.GetProperFirstName(code));
         }
 
-        [Fact]
-        public void TestFormatAsEmailAddress()
+        [Theory]
+        [InlineData(null,                          "")]
+        [InlineData("Givenname.Surname@Example.com",  "givenname.surname@example.com")]
+        [InlineData("givénname.sûrname@example.cöm",  "givenname.surname@example.com")]
+        [InlineData("givenname...surname@example.com", "givenname.surname@example.com")]
+        [InlineData("givenname.sur name@example.com",  "givenname.sur.name@example.com")]
+        [InlineData("givenname.sur-name@example.com",  "givenname.sur-name@example.com")]
+        public void TestFormatAsEmailAddress(string input, string expected)
         {
-            Assert.Equal("", ((string)null).FormatAsEmailAddress());                                         // #0
-            Assert.Equal("givenname.surname@example.com", "Givenname.Surname@Example.com".FormatAsEmailAddress());   // #1
-            Assert.Equal("givenname.surname@example.com", "givénname.sûrname@example.cöm".FormatAsEmailAddress());   // #2
-            Assert.Equal("givenname.surname@example.com", "givenname...surname@example.com".FormatAsEmailAddress()); // #3
-            Assert.Equal("givenname.sur.name@example.com", "givenname.sur name@example.com".FormatAsEmailAddress()); // #4
-            Assert.Equal("givenname.sur-name@example.com", "givenname.sur-name@example.com".FormatAsEmailAddress()); // #5
+            Assert.Equal(expected, input.FormatAsEmailAddress());
         }
 
-        [Fact]
-        public void TestFormatAsUsername()
+        [Theory]
+        [InlineData(null,               "")]
+        [InlineData("Givenname.Surname", "givenname.surname")]
+        [InlineData("givsur4",          "givsur4")]
+        [InlineData("givsür",           "givsur")]
+        [InlineData("givenname..surname", "givenname.surname")]
+        public void TestFormatAsUsername(string input, string expected)
         {
-            Assert.Equal("", ((string)null).FormatAsUsername());
-            Assert.Equal("givenname.surname", "Givenname.Surname".FormatAsUsername());
-            Assert.Equal("givsur4", "givsur4".FormatAsUsername());
-            Assert.Equal("givsur", "givsür".FormatAsUsername());
-            Assert.Equal("givenname.surname", "givenname..surname".FormatAsUsername());
+            Assert.Equal(expected, input.FormatAsUsername());
         }
 
-        [Fact]
-        public void TestFormatAsPersonnummer()
+        // Remember to update tests before new years 2099/2100 ;-)
+        [Theory]
+        [InlineData(null,             "")]
+        [InlineData("198909097788",   "198909097788")]
+        [InlineData("19890909-7788",  "198909097788")]
+        [InlineData("8909097788",     "198909097788")]
+        [InlineData("0109097788",     "200109097788")]
+        [InlineData("990909-7788",    "199909097788")]
+        [InlineData("000909-7788",    "200009097788")]
+        [InlineData("19890909TF88",   "19890909TF88")]
+        [InlineData("19890909-TF88",  "19890909TF88")]
+        [InlineData("890909TF88",     "19890909TF88")]
+        [InlineData("010909TF88",     "20010909TF88")]
+        public void TestFormatAsPersonnummer(string input, string expected)
         {
-            // Remember to update tests before new years 2099/2100 ;-)
-            Assert.Equal("", ((string)null).FormatAsPersonnummer());                // #0
-            Assert.Equal("198909097788", "198909097788".FormatAsPersonnummer());  // #1
-            Assert.Equal("198909097788", "19890909-7788".FormatAsPersonnummer()); // #2
-            Assert.Equal("198909097788", "8909097788".FormatAsPersonnummer());    // #3
-            Assert.Equal("200109097788", "0109097788".FormatAsPersonnummer());    // #4
-            Assert.Equal("199909097788", "990909-7788".FormatAsPersonnummer());   // #5
-            Assert.Equal("200009097788", "000909-7788".FormatAsPersonnummer());   // #6
-            Assert.Equal("19890909TF88", "19890909TF88".FormatAsPersonnummer());  // #7
-            Assert.Equal("19890909TF88", "19890909-TF88".FormatAsPersonnummer()); // #8
-            Assert.Equal("19890909TF88", "890909TF88".FormatAsPersonnummer());    // #9
-            Assert.Equal("20010909TF88", "010909TF88".FormatAsPersonnummer());    // #10
+            Assert.Equal(expected, input.FormatAsPersonnummer());
         }
 
-        [Fact]
-        public void TestFormatAsShortPersonnummer()
+        // Remember to update tests before new years 2099/2100 ;-)
+        [Theory]
+        [InlineData(null,             "")]
+        [InlineData("198909097788",   "890909-7788")]
+        [InlineData("19890909-7788",  "890909-7788")]
+        [InlineData("8909097788",     "890909-7788")]
+        [InlineData("0109097788",     "010909-7788")]
+        [InlineData("990909-7788",    "990909-7788")]
+        [InlineData("000909-7788",    "000909-7788")]
+        [InlineData("19890909TF88",   "890909-TF88")]
+        [InlineData("19890909-TF88",  "890909-TF88")]
+        [InlineData("890909TF88",     "890909-TF88")]
+        [InlineData("010909TF88",     "010909-TF88")]
+        public void TestFormatAsShortPersonnummer(string input, string expected)
         {
-            // Remember to update tests before new years 2099/2100 ;-)
-            Assert.Equal("", ((string)null).FormatAsPersonnummer());                              // #0
-            Assert.Equal("890909-7788", "198909097788".FormatAsShortPersonnummer());  // #1
-            Assert.Equal("890909-7788", "19890909-7788".FormatAsShortPersonnummer()); // #2
-            Assert.Equal("890909-7788", "8909097788".FormatAsShortPersonnummer());    // #3
-            Assert.Equal("010909-7788", "0109097788".FormatAsShortPersonnummer());    // #4
-            Assert.Equal("990909-7788", "990909-7788".FormatAsShortPersonnummer());   // #5
-            Assert.Equal("000909-7788", "000909-7788".FormatAsShortPersonnummer());   // #6
-            Assert.Equal("890909-TF88", "19890909TF88".FormatAsShortPersonnummer());  // #7
-            Assert.Equal("890909-TF88", "19890909-TF88".FormatAsShortPersonnummer()); // #8
-            Assert.Equal("890909-TF88", "890909TF88".FormatAsShortPersonnummer());    // #9
-            Assert.Equal("010909-TF88", "010909TF88".FormatAsShortPersonnummer());    // #10
+            Assert.Equal(expected, input.FormatAsShortPersonnummer());
         }
     }
 }
